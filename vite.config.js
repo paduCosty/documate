1625b2877b8f5811e1d1c5b82bc100f8bfa1fddb
Constantin <constantin@redwallmedia.com>
2026-03-30 12:03:29 +0000

    feat: initial commit

---
+++ vite.config.js
@@ -0,0 +1,27 @@
+// vite.config.js
+import { defineConfig } from 'vite';
+import laravel from 'laravel-vite-plugin';
+import react from '@vitejs/plugin-react';
+
+export default defineConfig({
+    plugins: [
+        laravel({
+            input: 'resources/js/app.jsx',
+            refresh: true,
+        }),
+        react(),
+    ],
+    // vite.config.js
+    server: {
+        host: true,
+        port: 5173,
+        strictPort: true,
+        origin: 'http://localhost:5174',
+        cors: true,                    // temporar, ca să simplificăm
+        hmr: {
+            host: 'localhost',
+            clientPort: 5174,
+        },
+        watch: { usePolling: true },
+    },
+});
