7dc1d3fbbad0f82ccf13140fdf10484ef5aa41ca
Constantin <constantin@redwallmedia.com>
2026-04-09 11:21:17 +0000

    update docker files

--- vite.config.js
+++ vite.config.js
@@ -4,6 +4,9 @@ import laravel from 'laravel-vite-plugin';
 import react from '@vitejs/plugin-react';
 
 export default defineConfig({
+    build: {
+        cssMinify: false,
+    },
     plugins: [
         laravel({
             input: 'resources/js/app.jsx',
